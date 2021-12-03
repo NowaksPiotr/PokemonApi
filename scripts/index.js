c8084bbc879614b79d13dfba49dd06e7b704dd0a
Szamponetti <nowak.spiotr@gmail.com>
2021-12-03 16:07:48 +0100

    Changed search form. Magnifying glass icon is now a clickable button.

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// const url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
+// fetch(url).then(response => response.json()).then(data => console.log(data));
+
+/* 
+1. User enter pokemon name.
+2. After pressing "enter" or touching magnifying glass icon, there will be sent request to pokeapi.
+  2.1 If no pokemon name was entered there will be random pokemons displayed.
+  2.2 If there will be error there will be error screen.
+3. Afrer succesfull response pokemon cards with number of pokemon, default front sprite, and pokemon name will be displayed.
+~~~~~~~ More features soon ~~~~~~~ 
+*/
+
+
